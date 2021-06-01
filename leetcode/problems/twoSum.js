bf0c37300bb3dbfcb916f4198bca6f0431764166
ThillaiNarayanan <judearasu>
2021-06-01 00:59:46 -0400

    feat(): leetcode - Two Sum

---
+++ leetcode/problems/twoSum.js
@@ -0,0 +1,19 @@
+/**
+ *  Two Sum - Easy - Array
+ *  https://leetcode.com/problems/two-sum/
+ */ 
+
+ var twoSum = function (nums, target){
+     let result  = [];
+     for(let i=0; i< nums.length; i++){
+         for(let j=i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target) {
+                result.push(i); 
+                result.push(j);
+            }
+         }
+     }
+     return result;
+ }
+
+ module.exports = twoSum;
