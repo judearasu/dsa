615c2bda2e9db3b9f77d2e4e5c1355c757df181b
judearasu <judearasu@gmail.com>
2021-09-01 11:17:48 -0400

    OR-3 : Rotate Array code commented

--- leetcode/problems/rotateArray.js
+++ leetcode/problems/rotateArray.js
@@ -8,15 +8,15 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
-    let i=0;
-    while(i<k){
-        let len = nums.length-i;
-        let popVal = nums.pop(nums[len-1]);
-        nums.unshift(popVal);
-        i++;
-    }
-    return nums;
-};
+// var rotate = function (nums, k) {
+//     let i=0;
+//     while(i<k){
+//         let len = nums.length-i;
+//         let popVal = nums.pop(nums[len-1]);
+//         nums.unshift(popVal);
+//         i++;
+//     }
+//     return nums;
+// };
 
 console.log(rotate(nums = [1,2,3,4,5,6,7], k = 3))
