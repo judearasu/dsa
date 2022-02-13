6dbb21cea0c1173c3620d532ee98cf75ab367273
judearasu <judearasu@gmail.com>
2022-02-12 23:48:26 -0500

    longest Palindrome brute force

--- leetcode/problems/strings/longestPalindromeSubstring.js
+++ leetcode/problems/strings/longestPalindromeSubstring.js
@@ -4,13 +4,45 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
-    const strLength = s.length();
-    if (strLength < 2) {
-        return s;
+    let newStr = '';
+    let left = 0;
+    let right = 0;
+    let max = 0;
+    let outPut = '';
+    while (right < s.length) {
+        newStr += s[right];
+        let res = checkPalindrome(newStr);
+        if (res) {
+            if (newStr.length > max) {
+                outPut = newStr;
+                max = newStr.length;
+            }
+        }
+        if (right === s.length - 1) {
+            newStr = '';
+            left = left + 1;
+            right = left;
+            continue;
+        }
+        right++;
     }
 
-
+    return outPut;
 
 };
 
+
+var checkPalindrome = function (str) {
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        if (str[left] === str[right]) {
+            left++;
+            right--;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
 console.log(longestPalindrome('babad'))
