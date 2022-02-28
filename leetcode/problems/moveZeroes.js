3a5be6d1ec32f77cf82368fa83319a4f0af8366d
judearasu <judearasu@gmail.com>
2022-02-27 23:47:06 -0500

    test cleanup

--- leetcode/problems/moveZeroes.js
+++ leetcode/problems/moveZeroes.js
@@ -9,14 +9,14 @@
 var moveZeroes = function (nums) {
     let lastNonZeroFoundAt = 0;
     for (let x = 0; x < nums.length; x++) {
-       if(nums[x] !== 0){
-           nums[lastNonZeroFoundAt++] = nums[x];
-       }
+        if (nums[x] !== 0) {
+            nums[lastNonZeroFoundAt++] = nums[x];
+        }
     }
-    for(let i = lastNonZeroFoundAt; i< nums.length;i++){
-        nums[i]= 0;
+    for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
+        nums[i] = 0;
     }
     return nums;
 };
 
-console.log(moveZeroes(nums = [0, 1, 0, 3, 12]))
+console.log(moveZeroes([0, 1, 0, 3, 12]))
