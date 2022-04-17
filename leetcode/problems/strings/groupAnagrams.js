6bea135f91ab3aa006108caf22a653aad067f5b7
judearasu <judearasu@gmail.com>
2022-04-17 09:08:55 -0400

    changes on group anagrams

--- leetcode/problems/strings/groupAnagrams.js
+++ leetcode/problems/strings/groupAnagrams.js
@@ -5,19 +5,19 @@
    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
  */
 
-var groupAnagrams = function(strs){
-    if(!strs || !strs.length){
+var groupAnagrams = function (strs) {
+    if (!strs || !strs.length) {
         return null;
     }
     let anagrams = {};
-    for(let x=0; x < strs.length; x++){
-        let sortedWord = strs[x].split('').sort().join();
-        if(sortedWord in anagrams){
+    for (let x = 0; x < strs.length; x++) {
+        const sortedWord = strs[x].split('').sort().join();
+        if (sortedWord in anagrams) {
             anagrams[sortedWord].push(strs[x])
-        }else{
+        } else {
             anagrams[sortedWord] = [strs[x]];
         }
     }
     return Object.values(anagrams);
 }
-console.log(groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
+console.log(groupAnagrams(strs = ["eat", "tea", "tan", "ate", "nat", "bat"]))
--- misc/strings/appendEmoji.js
+++ misc/strings/appendEmoji.js
@@ -7,18 +7,37 @@ function appendEmoji(text, emoji) {
     let outPutStr = '';
     let hasSeen = false;
     if (typeof text === 'object') {
-        text = text.toString().toLowerCase();
+        text = text.toString();
     }
     for (let startIdx = 0; startIdx < text.length; startIdx++) {
-        if (text[startIdx].match(/[a-z]/i)) {
-            outPutStr += text[startIdx];
-        } else {
+        if (text[startIdx].match(/\s/g)) {
             outPutStr += emoji;
             hasSeen = true;
+        } else {
+            outPutStr += text[startIdx];
+
         }
 
     }
     return hasSeen ? outPutStr : "invalid string";
 }
 
-console.log(appendEmoji('hello world', '🙃'))
+console.log(appendEmoji({ hello: 'world' }, '🙃'))
+
+
+
+// console.log(typeof message, message);
+// let outPutStr = '';
+// let hasSeen = false;
+// if(message){
+// message = message.toString();
+// for(let startIdx = 0; startIdx < message.length; startIdx++){
+//     if(message[startIdx].match(/\s/g)){
+//         outPutStr += emoji;
+//         hasSeen = true;
+//     }else{
+//         outPutStr +=message[startIdx];        
+//     }
+// }
+// }
+// return hasSeen ? outPutStr : "invalid string";
