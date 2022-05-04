454f897d046c8256874b3d9211e49285e0f92fa3
judearasu <judearasu@gmail.com>
2022-05-03 23:47:45 -0400

    leetCode():3: Longest substring without repeating characters - BT approach

--- leetcode/problems/array/maxArea.js
+++ leetcode/problems/array/maxArea.js
@@ -15,6 +15,19 @@ var maxArea = function (heights) {
     //     }
     // }
     // return maxArea;
+    let maxArea = 0, p1 = 0, p2 = heights.length - 1;
+    while (p1 < p2) {
+        const height = Math.min(heights[p1], heights[p2]);
+        const width = p2 - p1;
+        const area = height * width;
+        maxArea = Math.max(maxArea, area);
+        if (heights[p1] <= heights[p2]) {
+            p1++;
+        } else {
+            p2--;
+        }
+    }
+    return maxArea;
 }
 
 console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
