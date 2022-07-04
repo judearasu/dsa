9548a2ace78d4302f5c212aebf3bc3bacb1d6627
judearasu <judearasu@gmail.com>
2022-07-04 17:43:58 -0400

    KABO-3 # Number of steps to reduce a  number to zero

---
+++ leetcode/numberOfSteps.js
@@ -0,0 +1,17 @@
+/**
+ * 1342. Number of Steps to Reduce a Number to Zero
+ */
+var numberOfSteps = function (num) {
+    let steps = 0;
+    while(num >0){
+        if(num %2 === 0){
+            num = num /2;
+        }else{
+            num--;
+        }
+        steps++;
+    }
+    return steps;
+}
+
+console.log(numberOfSteps(14));
